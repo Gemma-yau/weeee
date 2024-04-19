260daba1d44b381515c5c30c131d9e49db5a3e9b
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-19 12:07:12 +0800

    Update printcolour.cpp

--- printcolour.cpp
+++ printcolour.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
 #include <vector>
+#include "printcolour.h"
 using namespace std;
 
+#define reset  "\033[0m"
 #define Green   "\033[32m"  
 #define Red   "\033[31m"      
 #define Yellow  "\033[33m"   
 
-struct coord{
-    int first;
-    int second;
-};
 
 void print(coord point,vector<coord> used,coord present){
     if (point.first == present.first && point.second==present.second){
-        cout<<Yellow<<".";
+        cout<<Yellow<<"."<<reset;
         return;
     }
     if (used.size()==0){
-        cout<<Green<<".";
-        return;}
+        cout<<Green<<"."<<reset;
+        return;
+	}
         
     else{
       for (int i=0;i<used.size();i++)
        if (used[i].first==point.first && used[i].second==point.second){
-           cout<<Red<<".";
+           cout<<Red<<"."<<reset;
            return;
        }
-       cout<<Green<<".";
+       cout<<Green<<"."<<reset;
        return;
     } 
-}
-
-int main(){
-  vector<coord> used = {{1,1},{2,0}};
-  coord present = {3,4};
-  print({1,1},used,present);
-  print({5,4},used,present);
-  print({3,4},used,present);
 }
