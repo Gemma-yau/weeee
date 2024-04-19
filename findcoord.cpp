a7fed5d4d8112e59aaf877026e77578d983982a9
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-19 16:12:19 +0800

    Create findcoord.cpp

---
+++ findcoord.cpp
@@ -0,0 +1,31 @@
+///to find if the coord is used ir not, return true if used.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct coord{
+    int first;
+    int second;
+};
+
+bool findcoord(coord point,vector<coord> used){
+ for (int i=0;i<used.size();i++){
+  if (used[i].first == point.first && used[i].second == point.second)
+   return true;}
+   return false;
+ }
+
+ int main(){
+  vector<coord> used={{0,0},{1,0}};
+  if (findcoord({1,1},used))
+  cout<<"yes";
+  else cout<<"no";
+  cout<<endl;
+
+  if (findcoord({0,0},used))
+  cout<<"yes";
+  else cout<<"no";
+  cout<<endl;
+ }
