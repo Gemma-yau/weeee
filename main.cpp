fad955eb384b44fd6b3189f2ddbea169923bd59c
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-21 14:22:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,3 +2,41 @@
 #include <>
 #using namespace std;
 
+
+int main() {
+
+while (true) {
+  setboard()；           //generate number  
+  string status=“normal” ;
+  int next=1;
+  coord present={0,0} ;
+  vector<coord> used = { {0,0} };
+
+//let them click enter to start 
+
+//print the main page of the game
+//receive the enter button
+
+  printbg();
+
+  while (status !=“end” && status !=“lose”) {
+     status=“normal” ;     #reset_the_content_of_status 
+     move();
+     printbg();
+     lose() ;   
+  }
+
+  if ( status == “lose” ) {         
+        ///print sth;
+    
+  } 
+
+  else {                   //status==end (wanna restart)
+        ///print sth
+
+  }
+
+}
+
+}
+
