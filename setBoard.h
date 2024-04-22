a0cc630e5d023223679ce0fdc6e8dd7cd8feb0f1
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-23 03:30:05 +0800

    Create setBoard.h

---
+++ setBoard.h
@@ -0,0 +1,14 @@
+#ifndef SETBOARD_H
+#define SETBOARD_H
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include "setBoard.h"
+
+using namespace std;
+
+vector < vector <int> > setBoard(int size);
+
+#endif
