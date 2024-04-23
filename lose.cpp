0622bc58850a70f6ba0b5c5195043b84649a6c70
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-23 22:20:29 +0800

    change to findcoord.h

--- lose.cpp
+++ lose.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include "move.h"
+#include "findcoord.h"
 #include "lose.h"
 
 using namespace std;
+
+bool findcoord(coord point,vector<coord> used);
 
 void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used) {
   string seq="1234321234321234";
