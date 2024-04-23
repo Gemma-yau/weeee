54774b4118f93a504af14294164d80d483b903e9
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-24 01:57:55 +0800

    Create printmainpage.cpp

---
+++ printmainpage.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+#define reset  "\033[0m"
+#define Yellow  "\033[33m"
+
+
+system("clear");
+  cout<<endl<<endl<<endl;
+  cout<<"     "<<"       "<<Yellow<<"•                                            "<<endl;
+  cout<<"     "<<"        \\                                            "<<reset<<endl;
+  cout<<"     "<<"   CCCCC "<<Yellow<<"\\"<<reset<<"OOOOO  N   N  N   N  EEEEE  CCCCC  TTTTT   "<<endl;
+  cout<<"     "<<"   C      O   O  NN  N  NN  N  E      C        T     "<<endl;
+  cout<<"     "<<"   C      O"<<Yellow<<"\\"<<reset<<"  O  N N N  N N N  EEEEE  C        T     "<<endl;
+  cout<<"     "<<"   C      O "<<Yellow<<"\\"<<reset<<" O  N  NN  N  NN  E      C        T     "<<endl;
+  cout<<"     "<<"   CCCCC  OOOOO  N   N  N   N  EEEEE  CCCCC    T   "<<Yellow<<"• "<<endl;
+  cout<<"     "<<"              \\                                   /  "<<reset<<endl;
+  cout<<"     "<<"  the          "<<Yellow<<"\\                                 /   "<<reset<<endl;
+  cout<<"     "<<"         D D    "<<Yellow<<"\\"<<reset<<"OOOOO  TTTTT  SSSSS            "<<Yellow<<"/"<<reset<<"    "<<endl;
+  cout<<"     "<<"         D   D   O   O    T    S               "<<Yellow<<"/     "<<reset<<endl;
+  cout<<"     "<<"         D    D  O"<<Yellow"\\"<<reset<<"  O    T    SSSSS          "<<Yellow<<"/      "<<reset<<endl;
+  cout<<"     "<<"         D   D   O "<<Yellow<<"\\"<<reset<<" O    T        S         "<<Yellow<<"/       "<<reset<<endl;
+  cout<<"     "<<"         D D     OOOOO    T    SSSSS        "<<Yellow<<"/        "<<endl;
+  cout<<"     "<<"                     \\                     /         "<<endl;
+  cout<<"     "<<"                      •                   •         "<<reset<<endl<<endl<<endl;
+  }
