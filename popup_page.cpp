1f390e5612ad6e50850dada097f7012296c4ad77
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-23 14:28:59 +0800

    Create popup_page.cpp

---
+++ popup_page.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void popup_page(){
+  system("clear");
+  cout<<endl<<endl<<endl;
+  cout<<"     ";
+  cout<<"#################################"<<endl;
+  cout<<"     "<<"#"<<"                               "<<"#"<<endl;
+  cout<<"     "<<"#"<<"   [      Return  (Q)      ]   "<<"#"<<endl;
+  cout<<"     "<<"#"<<"                               "<<"#"<<endl;
+  cout<<"     "<<"#"<<"                               "<<"#"<<endl;
+  cout<<"     "<<"#"<<"   [      Restart (R)      ]   "<<"#"<<endl;
+  cout<<"     "<<"#"<<"                               "<<"#"<<endl;
+  cout<<"     "<<"#"<<"                               "<<"#"<<endl;
+  cout<<"     "<<"#"<<"   [  Start a New Game (N) ]   "<<"#"<<endl;
+  cout<<"     "<<"#"<<"                               "<<"#"<<endl;
+  cout<<"     "<<"#################################"<<endl;
+  cout<<endl;
+  }
