9160fc97d8d4b779381ac36136b37962ae724a7d
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-23 16:07:43 +0800

    Create printresult.cpp

---
+++ printresult.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void printresult(string status){
+  system("clear");
+  if (status=="end"){
+   cout<<endl<<endl<<endl;
+   cout<<"     "<<"######################################"<<endl;
+   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    EEEEEE    N     N    D D        "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    E         NN    N    D   D      "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    E         N N   N    D    D     "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    EEEEEE    N  N  N    D     D    "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    E         N   N N    D    D     "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    E         N    NN    D   D      "<<"#"<<endl;
+   cout<<"     "<<"#"<<"    EEEEEE    N     N    D D        "<<"#"<<endl;
+   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
+   cout<<"     "<<"######################################"<<endl;
+   cout<<endl;
+  }
+
+  else if (status=="lose"){
+   cout<<endl<<endl<<endl;
+   cout<<"     "<<"######################################"<<endl;
+   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   L        O      SSSSS    EEEEE   "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   L       O O     S        E       "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   L      O   O    S        E       "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   L     O     O   SSSSS    EEEEE   "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   L      O   O        S    E       "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   L       O O         S    E       "<<"#"<<endl;
+   cout<<"     "<<"#"<<"   LLLLL    O      SSSSS    EEEEE   "<<"#"<<endl;
+   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
+   cout<<"     "<<"######################################"<<endl;
+   cout<<endl;
+  }
+
+  else {
+   cout<<endl<<endl<<endl;
+   cout<<"       "<<"🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉"<<endl;
+   cout<<"     "<<"  "<<"                                    "<<"  "<<endl;
+   cout<<"     "<<"🎉"<<"    W       W   IIIII    N     N    "<<"🎉"<<endl;
+   cout<<"     "<<"  "<<"    W       W     I      NN    N    "<<"  "<<endl;
+   cout<<"     "<<"🎉"<<"    W       W     I      N N   N    "<<"🎉"<<endl;
+   cout<<"     "<<"  "<<"    W   W   W     I      N  N  N    "<<"  "<<endl;
+   cout<<"     "<<"🎉"<<"    W  W W  W     I      N   N N    "<<"🎉"<<endl;
+   cout<<"     "<<"  "<<"     WW   WW      I      N    NN    "<<"  "<<endl;
+   cout<<"     "<<"🎉"<<"      W   W     IIIII    N     N    "<<"🎉"<<endl;
+   cout<<"     "<<"  "<<"                                    "<<"  "<<endl;
+   cout<<"       "<<"🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉"<<endl;
+   cout<<endl;
+  }
+}
