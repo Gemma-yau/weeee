638e3cddaf428f5cc6a67c8056b1218d89a2ad74
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-23 14:34:10 +0800

    Update lose.cpp

--- lose.cpp
+++ lose.cpp
@@ -2,22 +2,23 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include "move.h"
+#include "lose.h"
 
 using namespace std;
 
-void lose(string &status, int next, int size = 4, coord present, <vecotr<vecotr<int>> board, vector<coord> used) {
+void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used) {
   for ( int i = 0; i < size; i++ ){
-    if ( present.fisrt != board[i] ) {            //## not the same row as present ##
+    if ( present.first != i ) {            //## not the same row as present ##
       for ( int j = 0; j < size; j++ ){          //## for every element in that row ##
         coord step = {i,j};
-        if ( !find(used.begin(),used.end(),step) && board[i][j] == next ){      //## if not in used and value == next
+        if ( !findcoord(step,used) && board[i][j] == next ){      //## if not in used and value == next
           return;
         }
       }
     }
   }
-  status ="lose";
+  status = "lose";
   return;
-
-
 }
+
