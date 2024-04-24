158500449ba2d3ad242c4f723b726fd2dff6bdeb
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-24 11:30:57 +0800

    Create getch.cpp

---
+++ getch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+int getch() {
+    struct termios oldTermios, newTermios;
+    tcgetattr(STDIN_FILENO, &oldTermios);
+    newTermios = oldTermios;
+    newTermios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newTermios);
+    int ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldTermios);
+    return ch;
+}
