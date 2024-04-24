b8c9472d65d5194b60457cb62ecb8caead28af5b
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-24 12:06:52 +0800

    Update move.h

--- move.h
+++ move.h
@@ -11,7 +11,6 @@
 
 using namespace std;
 
-bool findcoord(coord point,vector<coord> used);
 bool canconnect(int next, vector< vector<int> > lst, coord present,coord previous,string &status);
 void popup_page();
 void move (vector<coord> &used, coord &present,int &next, vector< vector<int> > lst, string &status);
