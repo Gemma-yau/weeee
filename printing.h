f64445374597a5fb84ac71fd14b6e44b76f82601
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-24 12:07:33 +0800

    Create printing.h

---
+++ printing.h
@@ -0,0 +1,17 @@
+#ifndef PRINTBG_H
+#define PRINTBG_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>     //for clear screen
+#include "getch.h"
+#include "printcolour.h"            //for struct coord
+#include "findcoord.h"
+using namespace std;
+
+bool canconnect(int next, vector< vector<int> > lst, coord present,coord previous,string &status);
+void popup_page();
+void move (vector<coord> &used, coord &present,int &next, vector< vector<int> > lst, string &status);
+
+#endif
