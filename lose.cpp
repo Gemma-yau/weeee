b20185eabbc78fd29ebd0a5dff8971ff0a595819
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-26 17:31:33 +0800

    Update lose.cpp

--- lose.cpp
+++ lose.cpp
@@ -8,10 +8,9 @@
 using namespace std;
 
 
-void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used) {
-  string seq="1234321234321234";
+void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used, int seq[20]) {
   for ( int i = 0; i < size; i++ ){
-    if ( present.first != i ) {            //## not the same row as present ##
+    if ( i != present.first ) {            //## not the same row as present ##
       for ( int j = 0; j < size; j++ ){          //## for every element in that row ##
         coord step = {i,j};
         if ( !findcoord(step,used) && board[i][j] == seq[next] ){      //## if not in used and value == next
