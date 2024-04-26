5d271e2646e9a1e083823ca18d4cbf1978396319
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-26 17:31:53 +0800

    Update lose.h

--- lose.h
+++ lose.h
@@ -9,6 +9,6 @@
 
 using namespace std;
 
-void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used);
+void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used, int seq[20]);
 
 #endif
