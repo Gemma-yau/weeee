4eebbd6dc2defef286feb91c5d6d1ad273341d67
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-26 17:29:28 +0800

    Update move.h

--- move.h
+++ move.h
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-bool canconnect(int next, vector< vector<int> > lst, coord present,coord previous,string &status);
+bool canconnect(int next, vector< vector<int> > lst, coord present,string &status, vector<coord> used, int seq[20]);
 void popup_page();
-void move (vector<coord> &used, coord &present,int &next, vector< vector<int> > lst, string &status);
+void move (vector<coord> &used, coord &present,int &next, vector< vector<int> > lst, string &status, int seq[20]);
 
 #endif
