1cfecfdb35bb3d3ed95059b6bf1089a8dd080127
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 01:32:19 +0800

    Update findcoord.cpp

--- findcoord.cpp
+++ findcoord.cpp
@@ -1,13 +1,11 @@
-///to check if the dot has been used or not, return true if used.
-///input: the coordinates of dot you want to check, the vector storing all dots that have been used
-
 #include <iostream>
 #include <vector>
 #include "findcoord.h"
 #include "printcolour.h"   //for struct coord
 using namespace std;
 
-
+///to check if the dot has been used or not, return true if used.
+///input: the coordinates of dot you want to check, the vector storing all dots that have been used
 bool findcoord(coord point,vector<coord> used){
  for (int i=0;i<used.size();i++){
   if (used[i].first == point.first && used[i].second == point.second)
