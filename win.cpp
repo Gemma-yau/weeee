cc994e4992fccb4f405b67e445080c8270428d13
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 02:27:25 +0800

    Update win.cpp

--- win.cpp
+++ win.cpp
@@ -5,6 +5,8 @@
 #include "printcolour.h"
 using namespace std;
 
+/// to determine if the player has won or not
+/// it will change the status to "win" when all the dots have been used 
 void win(string &status, vector<coord> used){
   if (used.size() == 16)
    status="win";
