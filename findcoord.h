a8a19ba687f9bf240dd2569de743d907caba8577
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 01:25:15 +0800

    Update findcoord.h

--- findcoord.h
+++ findcoord.h
@@ -1,11 +1,6 @@
 #ifndef FINDCOORD_H
 #define FINDCOORD_H
 
-#include <iostream>
-#include <vector>
-#include "printcolour.h"
-using namespace std;
-
 bool findcoord(coord point,vector<coord> used);
 
 #endif
