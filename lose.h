af2b7dae5b82394a365aea29dd43d86f9997dddd
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 01:24:52 +0800

    Update lose.h

--- lose.h
+++ lose.h
@@ -1,14 +1,6 @@
 #ifndef LOSE_H
 #define LOSE_H
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include "findcoord.h"
-
-using namespace std;
-
 void lose(string &status, int next, int size, coord present, vector<vector<int>> board, vector<coord> used, int seq[20]);
 
 #endif
