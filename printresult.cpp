a3dfb85b6f26973a8870d4398ee1676122affd7f
melo <146823087+ktkkkkkkkk@users.noreply.github.com>
2024-04-29 19:22:26 +0800

    Update printresult.cpp

--- printresult.cpp
+++ printresult.cpp
@@ -1,60 +1,37 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 #include "printresult.h"
 #include "getch.h"
 using namespace std;
 
 void printresult(string status){
   system("clear");
+  ifstream fin;
+  string line;
   if (status=="end"){
    cout<<endl<<endl<<endl<<endl<<endl;
-   cout<<"     "<<"######################################"<<endl;
-   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    EEEEEE    N     N    D D        "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    E         NN    N    D   D      "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    E         N N   N    D    D     "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    EEEEEE    N  N  N    D     D    "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    E         N   N N    D    D     "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    E         N    NN    D   D      "<<"#"<<endl;
-   cout<<"     "<<"#"<<"    EEEEEE    N     N    D D        "<<"#"<<endl;
-   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
-   cout<<"     "<<"######################################"<<endl;
-   cout<<endl;
+   fin.open("end.txt");
   }
 
   else if (status=="lose"){
    cout<<endl<<endl<<endl<<endl<<endl;
-   cout<<"     "<<"######################################"<<endl;
-   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   L       OOOOO   SSSSS    EEEEE   "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   L       O   O   S        E       "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   L       O   O   S        E       "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   L       O   O   SSSSS    EEEEE   "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   L       O   O       S    E       "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   L       O   O       S    E       "<<"#"<<endl;
-   cout<<"     "<<"#"<<"   LLLLL   OOOOO   SSSSS    EEEEE   "<<"#"<<endl;
-   cout<<"     "<<"#"<<"                                    "<<"#"<<endl;
-   cout<<"     "<<"######################################"<<endl;
-   cout<<endl;
+   fin.open("lose.txt");
   }
 
   else {
    cout<<endl<<endl<<endl<<endl<<endl;
-   cout<<"       "<<"🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉"<<endl;
-   cout<<"     "<<"  "<<"                                    "<<"  "<<endl;
-   cout<<"     "<<"🎉"<<"    W       W   IIIII    N     N    "<<"🎉"<<endl;
-   cout<<"     "<<"  "<<"    W       W     I      NN    N    "<<"  "<<endl;
-   cout<<"     "<<"🎉"<<"    W       W     I      N N   N    "<<"🎉"<<endl;
-   cout<<"     "<<"  "<<"    W   W   W     I      N  N  N    "<<"  "<<endl;
-   cout<<"     "<<"🎉"<<"    W  W W  W     I      N   N N    "<<"🎉"<<endl;
-   cout<<"     "<<"  "<<"     WW   WW      I      N    NN    "<<"  "<<endl;
-   cout<<"     "<<"🎉"<<"      W   W     IIIII    N     N    "<<"🎉"<<endl;
-   cout<<"     "<<"  "<<"                                    "<<"  "<<endl;
-   cout<<"       "<<"🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉"<<endl;
-   cout<<endl;
+   fin.open("win.txt");
   }
+
+  while ( getline(fin,line) ){
+     cout << line << endl;}
+  cout<<endl;
+  fin.close();
+
+  usleep(1000000);
+  cout << "                            [PRESS ENTER TO CONTINUE]" << endl;
   int get=getch();
   while (get != 10)
     get=getch();
 }
-
