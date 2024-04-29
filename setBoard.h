e4589800ad536f4a9608a5dda9a5207a348a684b
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 01:23:27 +0800

    Update setBoard.h

--- setBoard.h
+++ setBoard.h
@@ -1,13 +1,6 @@
 #ifndef SETBOARD_H
 #define SETBOARD_H
 
-#include <iostream>
-#include <vector>
-#include <stdlib.h>
-#include <time.h>
-
-using namespace std;
-
 vector < vector < int >> setBoard(int Number[20]);
 
 #endif
