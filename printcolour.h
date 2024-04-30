4ce05e15f167273212dceb31c1c38dbfd4eec6e9
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 14:01:41 +0800

    Update printcolour.h

--- printcolour.h
+++ printcolour.h
@@ -1,6 +1,13 @@
 #ifndef PC_H
 #define PC_H
 
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <unistd.h>
+#include "getch.h"
+using namespace std;
+
 using namespace std;
 struct coord{
     int first;
