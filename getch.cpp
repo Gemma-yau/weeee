5ae78c312e4feea0cb3790f39290e80f75272068
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 11:08:55 +0800

    Update getch.cpp

--- getch.cpp
+++ getch.cpp
@@ -3,15 +3,15 @@
 #include <unistd.h>
 #include "getch.h"
 
-/// to receive keyboard input from the player without displaying/printing the input on screen
+/// to receive keyboard input from the player without displaying/printing it on the screen
 /// return the ASCII value of the input
 int getch() {
-    struct termios oldTermios, newTermios;
-    tcgetattr(STDIN_FILENO, &oldTermios);
-    newTermios = oldTermios;
-    newTermios.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newTermios);
-    int ch = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldTermios);
-    return ch;
+    struct termios oldtermios, newtermios ;
+    tcgetattr(STDIN_FILENO, &oldtermios) ;      //store the old one
+    newtermios = oldtermios ;
+    newtermios.c_lflag &= ~(ICANON | ECHO);        //change it
+    tcsetattr(STDIN_FILENO, TCSANOW, &newtermios);    //use the new one
+    int value = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldtermios);           //switch back the old one
+    return value;
 }
