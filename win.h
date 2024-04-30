50222b37a9c6af25c758fa1874861e415b94c319
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 14:02:57 +0800

    Update win.h

--- win.h
+++ win.h
@@ -1,9 +1,10 @@
 #ifndef WIN_H
 #define WIN_H
 
-#include "printcolour.h"
-#include <string>
+#include <iostream>
 #include <vector>
+#include <string>
+#include "printcolour.h"
 using namespace std;
 
 void win(string &status, vector<coord> used);
