efc4442746d74ad23447802da36649b70295ea05
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 14:02:06 +0800

    Update printresult.h

--- printresult.h
+++ printresult.h
@@ -1,7 +1,12 @@
 #ifndef PRINTRESULT_H
 #define PRINTRESULT_H
 
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include "getch.h"
 using namespace std;
+
 void printresult(string status);
 
 #endif
