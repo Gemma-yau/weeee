5e1041ef050d31e3f9fb8ed977792eddaa91111f
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 14:01:25 +0800

    Update printbg.h

--- printbg.h
+++ printbg.h
@@ -1,6 +1,15 @@
 #ifndef PRINTBG_H
 #define PRINTBG_H
 
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include "printcolour.h" 
+using namespace std;
+
 void printbg(string status,coord present,vector<coord> used,int next, vector < vector<int>> lst, int mode );
 
 #endif
