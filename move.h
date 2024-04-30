aa072cad9467907194f9e4432d8b8da95b30188b
Gemma-yau <159634965+Gemma-yau@users.noreply.github.com>
2024-04-30 14:01:06 +0800

    Update move.h

--- move.h
+++ move.h
@@ -1,6 +1,16 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>     
+#include "getch.h"
+#include "printcolour.h"            
+#include "findcoord.h"
+using namespace std;
+
 bool canconnect(int next, vector< vector<int> > lst, coord present,string &status, vector<coord> used, int seq[20]);
 void popup_page();
 void move (vector<coord> &used, coord &present,int &next, vector< vector<int> > lst, string &status, int seq[20]);
